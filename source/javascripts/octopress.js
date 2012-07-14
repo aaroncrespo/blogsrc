e76d42462141a6c2d5d4e9f6a561e61d06286226
Aaron Crespo <aaroncrespo@gmail.com>
2012-07-14 19:18:17 -0400

    More sass gone Added an about page. some js cleanup

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -1,17 +1,17 @@
-function getNav() {
-  var mobileNav = $('nav[role=navigation] fieldset[role=search]').after('<fieldset class="mobile-nav"></fieldset>').next().append('<select></select>');
-  mobileNav.children('select').append('<option value="">Navigate&hellip;</option>');
-  $('ul[role=main-navigation]').addClass('main-navigation');
-  $('ul.main-navigation a').each(function(link) {
-    mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
-  });
-  $('ul.subscription a').each(function(link) {
-    mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
-  });
-  mobileNav.children('select').bind('change', function(event) {
-    if (event.target.value) { window.location.href = event.target.value; }
-  });
-}
+// function getNav() {
+//   var mobileNav = $('nav[role=navigation] fieldset[role=search]').after('<fieldset class="mobile-nav"></fieldset>').next().append('<select></select>');
+//   mobileNav.children('select').append('<option value="">Navigate&hellip;</option>');
+//   $('ul[role=main-navigation]').addClass('main-navigation');
+//   $('ul.main-navigation a').each(function(link) {
+//     mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
+//   });
+//   $('ul.subscription a').each(function(link) {
+//     mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
+//   });
+//   mobileNav.children('select').bind('change', function(event) {
+//     if (event.target.value) { window.location.href = event.target.value; }
+//   });
+// }
 
 function testFeatures() {
   var features = ['maskImage'];
@@ -70,6 +70,23 @@ $.domReady(function() {
   testFeatures();
   wrapFlashVideos();
   flashVideoFallback();
+  
+  
+  
+  $('nav a').hover(
+    function(){
+      var hr = $('.large');
+      if (this.text === 'Posts') {
+        hr.addClass('red');
+      } else if (this.text === 'Archives') {
+        hr.addClass('yellow');
+      } else if (this.text === 'About') {
+        hr.addClass('blue');
+      }
+    },
+    function(){
+      $('.large').attr('class','large');
+    });
 });
 
 // iOS scaling bug fix
